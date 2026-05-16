f5f8065d4716719b74685396d0a3e625cc2c1168
Yifei Zhao <49413602+zhao-yifei-1818@users.noreply.github.com>
2026-05-15 20:38:12 -0700

    dnc

---
+++ Divide_n_Conquer_Enumeration.py
@@ -0,0 +1,14 @@
+'''closestSubarray(arr, l, r)
+
+    if only one element:
+        return that element
+
+    split into left and right
+
+    solve left recursively
+    solve right recursively
+
+    solve crossing case
+
+    return best among the 3
+    '''
